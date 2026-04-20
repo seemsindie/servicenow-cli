f3a0fecad70213f3bb0fc43af48ca7128c28f1ac
Ivan Stamenkovic <ivan@seemsindie.com>
2026-04-20 04:05:23 +0200

    chore: release v0.5.4

--- src/constants.ts
+++ src/constants.ts
@@ -10,7 +10,7 @@ import { join } from "path";
 
 export const BIN_NAME = "sn";
 export const DISPLAY_NAME = "servicenow-cli";
-export const VERSION = "0.5.3";
+export const VERSION = "0.5.4";
 
 /**
  * XDG config home (spec: ~/.config by default).
